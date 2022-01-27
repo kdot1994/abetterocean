c46c03173c9ec65bbc0f6d7d9663ca642aa5bc96
Kilian Sprenkamp <kiliansprenkamp@Kilians-MacBook-Pro.local>
2022-01-27 19:42:05 +0100

    contract agreement sending ETH + fee

--- contracts/ABO.sol
+++ contracts/ABO.sol
@@ -8,20 +8,22 @@ contract ABO {
     // Declaring variable
     address public betterOcean = 0x86D15B422D924D9115986Dd7A87ee794CAF2cbaF;
     mapping (address => bool) public ngoWallets;
-    uint public nextProjectId = 0; //needs to be private later
+    mapping (uint => abo_contract) public projectIdToContract;
+    uint public nextProjectId = 0;
+    uint public aboFeeInPercent = 2;
 
 
     struct abo_contract{
         uint projectID;
         address ngoAddress;
-        address donorID;
+        address donorAddress;
         uint donation;
         string pointer;
         //bool paid;
         //string label;
     }
 
-    abo_contract[] public abo_contracts;
+    // abo_contract[] public abo_contracts;
 
     modifier isBetterOcean() {
         require(msg.sender == betterOcean);
@@ -38,16 +40,35 @@ contract ABO {
     }
 
     function createContract(uint _donation, string memory _pointer) isValidNgo public {
-        abo_contracts.push(abo_contract(nextProjectId++, msg.sender, address(0), _donation, _pointer));
+        uint projectId = nextProjectId++;
+        projectIdToContract[projectId] = abo_contract(projectId, msg.sender, address(0), _donation, _pointer);
     }
+
+    function donate(uint _projectId) public payable{
+        require(msg.value == projectIdToContract[_projectId].donation);
+        address ngoAddress  = projectIdToContract[_projectId].ngoAddress;        
+        uint aboFee = msg.value * aboFeeInPercent / 100; 
+        uint ngoAmount = msg.value - aboFee; 
+        // make a payment to NGO
+        payable(ngoAddress).transfer(ngoAmount);
+        // make a payment ABO
+        payable(betterOcean).transfer(aboFee);
+        abo_contract storage myContract = projectIdToContract[_projectId];
+        myContract.donorAddress = msg.sender;
+    } 
+
 
     function getContracts() public view returns (abo_contract[] memory) {
-        return abo_contracts;
+        abo_contract [] memory memoryArray = new abo_contract[](nextProjectId);
+        for(uint i = 0; i < nextProjectId; i++) {
+            memoryArray[i] = projectIdToContract[i];
+        }
+        return memoryArray;
     }
 
-    function getContract() public view returns (abo_contract memory) {
-        return abo_contracts[0];
-    }
+    // function getContract(uint _index) public view returns (abo_contract memory) {
+    //     return abo_contracts[_index];
+    // }
 
     function getNextProjectId() public view returns (uint) {
         return nextProjectId;
